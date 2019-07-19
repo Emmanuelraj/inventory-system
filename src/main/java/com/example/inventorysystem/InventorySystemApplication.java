c9c129dea95efc1df64491fd72f77e96c49d33c5
Emmanuel <immanuel.raj18@gmail.com>
2019-07-19 11:01:53 +0530

    On branch docker-inventory-system Your branch is up-to-date with 'origin/docker-inventory-system'.

--- src/main/java/com/example/inventorysystem/InventorySystemApplication.java
+++ src/main/java/com/example/inventorysystem/InventorySystemApplication.java
@@ -2,14 +2,21 @@ package com.example.inventorysystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.example.inventorysystem.repository.ProductRepository;
 
-
+/**
+ * 
+ * @author Emmanuel Raj
+ * This class is the entry point of an application 
+ *   
+ */
 
 @EnableJpaRepositories(basePackageClasses= ProductRepository.class)
 @SpringBootApplication
+@EnableEurekaClient
 public class InventorySystemApplication  {
 
 	public static void main(String[] args) {
